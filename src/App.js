c27d82686a8246c0948eae484600dfc695532fb0
eric shin <ericshin7824@gmail.com>
2022-06-19 21:12:18 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+function Nav() {
+    return (
+        <nav className="Global-nav">
+            <div>
+                <a href="/">Hn.</a>
+                <ul>
+                    <li>
+                        <a href="/">About</a>
+                    </li>
+                    <li>
+                        <a href="/">Tech</a>
+                    </li>
+                    <li>
+                        <a href="/">Work</a>
+                    </li>
+                    <li>
+                        <a href="/">Service</a>
+                    </li>
+                    <li>
+                        <a href="/">Content</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+function Header() {
+    return (
+        <header className="Main-header">
+            <h1>
+                <span>Eric Shin</span>
+                <span>Front - End</span>
+                <span>Developer</span>
+            </h1>
+        </header>
+    );
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav></Nav>
+            <Header></Header>
+        </div>
+    );
 }
 
 export default App;
